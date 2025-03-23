ebe00b60dfe8d565fd8915f7c3977ce0978e41bc
Nigel Robbins <nigel_robbins@hotmail.com>
2025-03-23 15:51:15 +0000

    again

--- process.py
+++ process.py
@@ -174,7 +174,7 @@ def process_questions(doc, extracted_text, questions, land_charges_configs, sect
             section_name = question.get("section", section_name)
         if not logged_section:
             #doc.add_paragraph(section_name, style="Heading 2")
-            logged_section = True
+            logged_section = False
         added_section = False
         if question["search_pattern"] in extracted_text:
             if not added_section and not logged_section:
