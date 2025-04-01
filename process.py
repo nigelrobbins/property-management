3953410676d80f7af8d6c6d0dd5e3f60ce337faf
Nigel Robbins <nigel_robbins@hotmail.com>
2025-04-01 15:30:37 +0100

    dont repeat  hdr

--- process.py
+++ process.py
@@ -258,12 +258,12 @@ def process_document_content(doc, yaml_data, extracted_text):
     
     # Process each document section
     for doc_section in yaml_data['docs']:
-        doc.add_heading(doc_section['heading'], level=1)
         
         # Check if identifier exists in text
         identifier = doc_section.get('identifier', '')
         if identifier and identifier in extracted_text:
             doc.add_paragraph(doc_section['message_if_identifier_found'])
+            doc.add_heading(doc_section['heading'], level=1)
         else:
             continue  # Skip processing this file if identifier not found
         
